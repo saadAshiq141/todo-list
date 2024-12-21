0203bb494b3b82b7e2917e58bd0a5b5e86f6f2b6
saadAshiq141 <saadashiq141@gmail.com>
2024-12-21 15:12:32 +0500

    UI Enhancement

--- index.php
+++ index.php
@@ -15,6 +15,8 @@ if(isset($_GET['submit'])){
     $task_name = $_GET['task_name'];
     $sql ="INSERT INTO `tasks`(`name`, `status`) VALUES ('$task_name','pending')";
     $results = $conn->query($sql);
+    header('Location: '.'index.php');
+
 }
 
 $sql = "SELECT * FROM `tasks` WHERE 1";
@@ -37,7 +39,10 @@ if(isset($_GET['id'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To-Do List</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -46,7 +51,7 @@ if(isset($_GET['id'])){
             padding: 0;
         }
         .container {
-            max-width: 600px;
+            max-width: 700px;
             margin: 50px auto;
             background: #fff;
             padding: 20px;
@@ -97,7 +102,7 @@ if(isset($_GET['id'])){
             background: #d9534f;
             border: none;
             color: white;
-            /* padding: 5px 10px; */
+            padding: 5px 10px; */
             border-radius: 4px;
             cursor: pointer;
         }
@@ -106,9 +111,10 @@ if(isset($_GET['id'])){
         }
         .completed{
             color: white;
-            background: #4CAE4C;
+            background-color: #5cb85c;
             padding: 5px;
             border-radius: 6px;
+
         }
         .delete{ 
             color: white;
@@ -139,37 +145,45 @@ if(isset($_GET['id'])){
             width: 300px;
             height: 50px;
         }
+        .Mark-as-complete{
+            width: 80px;
+            margin-left: 50px;
+            border: 1px solid red;
+            color: white;
+            background: green;
+            padding: 5px;
+            border-radius: 6px;
+        }
     </style>
 </head>
 <body>
     <div class="container">
-        <h1>To-Do List</h1>
-        <!-- Form for creating a task -->  
-        <form method="GET" action="index.php">
-            <input type="text" id="task-input" name="task_name" placeholder="Add a new task" required>
-            <button type="submit" name="submit">Add</button>
-        </form>
-        <!-- For Task Listing Display -->  
-        <ul id="todo-list">
-           <?php foreach($results as $value){ ?> 
-            
-            <li><div class="tasks"><?php echo $value['name']; ?></div>
-            <div class="edit-div">
-            <a href="edit.php?id=<?php echo $value['id'] ?>"  class="edit">Edit</a>
-            </div>
-            <div class="delete-div">
-            <a href="index.php?id=<?php echo $value['id'] ?>"  class="delete">Delete</a>
-            </div>
-            <?php if ($value['status']== 'pending'){ ?>
-    
-              <a href="markAsComplete.php?id=<?php echo $value['id'] ?>"  class="btn btn-danger">Mark As Complete</a> 
-            <?php } else{ ?>
-                    <p class="completed">Completed</p>  
-                 <?php } ?>
-        </li>
-            
-           <?php } ?>   
-        </ul>
+    <h1>To-Do List</h1>
+            <!-- Form for creating a task -->  
+            <form method="GET" action="index.php">
+                <input type="text" id="task-input" name="task_name" placeholder="Add a new task" required>
+                <button type="submit" name="submit">Add</button>
+            </form>
+            <!-- For Task Listing Display -->  
+            <ul id="todo-list">
+            <?php foreach($results as $value){ ?> 
+                
+                <li><div class="tasks"><?php echo $value['name']; ?> <span class= <?php if ($value['status'] == 'completed') { ?> "badge badge-success" <?php } else  { ?> "badge badge-warning" <?php } ?> completed"><?php echo $value['status'] ?></span> </div>
+                <div class="edit-div">
+                <a href="edit.php?id=<?php echo $value['id'] ?>"  class="btn btn-info">Edit</a>
+                </div>
+                <div class="delete-div">
+                <a href="index.php?id=<?php echo $value['id'] ?>"  class="btn btn-danger">Delete</a>
+                </div>
+                <?php if ($value['status']== 'pending'){ ?>
+        
+                <a href="markAsComplete.php?id=<?php echo $value['id'] ?>"  class="btn btn-success">Mark As Complete</a> 
+                <?php } ?>
+            </li>
+                
+            <?php } ?>   
+            </ul>
+
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 
